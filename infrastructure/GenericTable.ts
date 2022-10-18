51573177385223297387461a5aa1fab9c82f8884
thugiang <xanhtham.cuc@gmail.com>
2022-10-18 16:26:43 +1300

    link the function to API gateway. create endpoint for GET method for that function through API gateway. also create a table for dynamodb

---
+++ infrastructure/GenericTable.ts
@@ -0,0 +1,37 @@
+import { Stack } from "aws-cdk-lib";
+import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb"
+
+
+// GenericTable is used to declutter PropertyStack.ts. dedicate to make a table in dynamo
+
+
+export class GenericTable {
+    private name: string;
+    private primaryKey: string;
+    private stack: Stack;
+    private table: Table;
+
+    // assign the types for each arguments
+    public constructor(name: string, primaryKey: string, stack: Stack) {
+        this.name = name;
+        this.primaryKey = primaryKey;
+        this.stack = stack;
+        this.initialize()
+    }
+
+    // seperate to organize logic
+    private initialize() {
+        this.createTable()
+    }
+
+    //doc: aws-cdk/aws-dynamodb
+    private createTable() {
+        this.table = new Table(this.stack, this.name, {
+            partitionKey: {
+                name: this.primaryKey,
+                type: AttributeType.STRING
+            },
+            tableName: this.name
+        })
+    }
+}
--- infrastructure/PropertyStack.ts
+++ infrastructure/PropertyStack.ts
@@ -1,24 +1,36 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { RestApi, LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import {
   Code,
   Function as LambdaFunction,
   Runtime,
-
 } from "aws-cdk-lib/aws-lambda"; //Runtime = lambda.Runtime, Code = lambda.Ccode
 import { Construct } from "constructs";
 import { join } from "path";
-
+import { GenericTable } from "./GenericTable";
 
 export class PropertyStack extends Stack {
+  private api = new RestApi(this, "PropertyApi"); // create a RestApi method
+
+  //pas arguments to generictable : name, primaryKey, stack: this
+  private propertyTable = new GenericTable("PropertyTable", "propertyId", this);
+
   constructor(scope: Construct, id: string, props: StackProps) {
     // use scope and id from super class Stack
     super(scope, id, props);
 
-      // create a lambda function in clouFormation called helloLambda
-      const helloLambda = new LambdaFunction(this, 'helloLambda' , {
-          runtime: Runtime.NODEJS_16_X,
-          code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')), // path to deployed file
-          handler: 'hello.main'
-      })
+    // create a lambda function in clouFormation called helloLambda
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")), // path to deployed file
+      handler: "hello.main",
+    });
+
+    // link helloLambda to API gateway : GET, PUT, POST, DELETE. like backend
+    const helloLambdaIntergration = new LambdaIntegration(helloLambda);
+    // https://19ufnv5k3b.execute-api.ap-southeast-2.amazonaws.com/prod/hello -> "hello from Lambda!"
+    const helloLambdaResource = this.api.root.addResource("hello");
+    // add get method
+    helloLambdaResource.addMethod("GET", helloLambdaIntergration);
   }
 }
